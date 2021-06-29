1f71420349d9ba5a767bea5f28d4a22ad4fddb1d
Marko Laakso <marko.laakso@valuemotive.com>
2021-06-29 12:23:31 +0300

    Fixed version

--- MeasureDistance/MeasureDistance.ino
+++ MeasureDistance/MeasureDistance.ino
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
+#define MAX_READ 8190
+
 Servo servo;
 VL53L0X sensor;
 int distance = 0;
@@ -10,13 +12,11 @@ int distMax = 1;
 void setup() {
   servo.attach(3);
 
-  Serial.begin(9600);
   Wire.begin();
 
   sensor.setTimeout(500);
   if (!sensor.init()) {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
+    while (1) {}  // TODO: blink!
   }
 
   sensor.startContinuous();
@@ -24,10 +24,14 @@ void setup() {
 
 void loop() {
   distance = sensor.readRangeContinuousMillimeters();
-  Serial.print(distance);
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  Serial.println();
-  distMax = max(distance, distMax);
+  if (sensor.timeoutOccurred()) {
+    servo.write(0);
+  }
+  if (distance >= MAX_READ) {
+    distance = distMax;
+  } else {
+    distMax = max(distance, distMax);
+  }
 
   int angle = map(distance, 0, distMax, 0, 180);
   servo.write(angle);
